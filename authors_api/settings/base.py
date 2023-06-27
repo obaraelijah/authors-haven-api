aa9c5a4a0762ea0af807c1bbf913ae06a4b8b034
elijah obara <elijahobara357@gmail.com>
2023-06-27 19:12:43 +0300

    chore: logger config

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -195,3 +195,23 @@ DJOSER = {
         "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
 }
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
