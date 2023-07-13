3b3f3179238bcf7e9d0bfd90c006dbce109de43c
elijah obara <elijahobara357@gmail.com>
2023-07-13 18:03:34 +0300

    feat: add profile views

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+#TODO: change this in production
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from rest_framework import generics, status
+from rest_framework.exceptions import NotFound
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from.exceptions import CantFollowYourself
+from .models import Profile
+from .pagination import ProfilePagination
+from .renderers import ProfileJSONRenderer, ProfilesJSONRenderer
+from .serializers import FollowingSerializer, ProfileSerializer, UpdateProfileSerializer
+
+User = get_user_model()
+
+
+class ProfileListAPIView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    pagination_class = ProfilePagination
+    renderer_classes = [ProfilesJSONRenderer]  
+    
+class ProfileDetailAPIView(generics.RetrieveAPIView):
+    permission_classes  = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+    renderer_classes = [ProfileJSONRenderer]
+    
+    def get_queryset(self):
+       queryset = Profile.objects.select_related("user")
+       return queryset
+   
+    def get_object(self):
+        """retrieve a specic profile 
+        """
+        user = self.request.user
+        profile = self.get_queryset().get(user=user)
+        return profile
+    
+
+class UpdateProfileAPIView(generics.RetrieveAPIView):
+    serializer_class = UpdateProfileSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    renderer_classes = [ProfileJSONRenderer]
+    
+    def get_object(self):
+        profile = self.request.user.profile
+        return profile
+    
+    def patch(self, request, *args, **kwargs):
+        """handles Http patch request
+        """
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save() 
+        return Response(serializer.data, status=status.HTTP_200_OK)   
+    
+
