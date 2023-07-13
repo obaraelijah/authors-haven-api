d2eee488f166fa2ec88d575529a593cda02eb9a0
elijah obara <elijahobara357@gmail.com>
2023-07-13 18:57:46 +0300

    feat: add profile view

--- core_apps/profiles/views.py
+++ core_apps/profiles/views.py
@@ -60,3 +60,73 @@ class UpdateProfileAPIView(generics.RetrieveAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)   
     
 
+class FollowerListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        try:
+            profile = Profile.objects.get(user__id=request.user.id)
+            follower_profiles = profile.followers.all()
+            serializer = FollowingSerializer(follower_profiles, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "followers_count": follower_profiles.count(),
+                "followers": serializer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=404)
+        
+
+class FollowingListView(APIView):
+    def get(self, request, user_id, format=None):
+        try:
+            profile = Profile.objects.get(user__id=user_id)
+            following_profiles = profile.following.all()
+            users = [p.user for p in following_profiles]
+            serializer = FollowingSerializer(users, many=True)
+            formatted_response = {
+                "status_code": status.HTTP_200_OK,
+                "following_count": following_profiles.count(),
+                "users_i_follow": serializer.data,
+            }
+            return Response(formatted_response, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response(status=404)
+
+
+class FollowAPIView(APIView):
+    def post(self, request, user_id, format=None):
+        try:
+            follower = Profile.objects.get(user=self.request.user)
+            user_profile = request.user.profile
+            profile = Profile.objects.get(user__id=user_id)
+
+            if profile == follower:
+                raise CantFollowYourself()
+
+            if user_profile.check_following(profile):
+                formatted_response = {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": f"You are already following {profile.user.first_name} {profile.user.last_name}",
+                }
+                return Response(formatted_response, status=status.HTTP_400_BAD_REQUEST)
+
+            user_profile.follow(profile)
+            subject = "A new user follows you"
+            message = f"Hi there, {profile.user.first_name}!!, the user {user_profile.user.first_name} {user_profile.user.last_name} now follows you"
+            from_email = request.user.email
+            recipient_list = [profile.user.email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+            return Response(
+                {
+                    "status_code": status.HTTP_200_OK,
+                    "message": f"You are now following {profile.user.first_name} {profile.user.last_name}",
+                },
+            )
+        except Profile.DoesNotExist:
+            raise NotFound("You can't follow a profile that does not exist.")  
+                
+
+    
+       
