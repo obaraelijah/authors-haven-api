dfd63372df00606d7c28eddc3ae51f4c679c013c
elijah obara <elijahobara357@gmail.com>
2023-07-14 19:01:06 +0300

    feat: add bookmark view

--- core_apps/bookmarks/views.py
+++ core_apps/bookmarks/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from uuid import UUID
 
-# Create your views here.
+from django.db import IntegrityError
+from rest_framework import generics, permissions
+from rest_framework.exceptions import NotFound, ValidationError
+
+from core_apps.articles.models import Article
+
+from .models import Bookmark
+from .serializers import BookmarkSerializer
+
+
+class BookmarkCreateView(generics.CreateAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        article_id = self.kwargs.get("article_id")
+
+        if article_id:
+            try:
+                article = Article.objects.get(id=article_id)
+            except Article.DoesNotExist:
+                raise ValidationError("Invalid article_id provided")
+        else:
+            raise ValidationError("article_id is required")
+        try:
+            serializer.save(user=self.request.user, article=article)
+        except IntegrityError:
+            raise ValidationError("You have already bookmarked this article")
