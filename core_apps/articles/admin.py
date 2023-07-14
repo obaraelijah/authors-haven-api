b1c03c1a79ce30eaeb0d5ff82b06ca778a25e830
elijah obara <elijahobara357@gmail.com>
2023-07-14 12:17:07 +0300

    feat: add bookmark serilaizers and renderers

--- core_apps/articles/admin.py
+++ core_apps/articles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "author", "title", "slug", "view_count"]
+    list_display_links = ["pkid", "author"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["title", "body", "tags"]
+    ordering = ["-created_at"]
+
+
+class ArticleViewAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "article", "user", "viewer_ip"]
+    list_display_links = ["pkid", "article"]
+    list_filter = ["created_at", "updated_at"]
+    search_fields = ["article", "user", "viewer_ip"]
+
+
+class ClapAdmin(admin.ModelAdmin):
+    list_display = ["pkid", "id", "user", "article"]
+    list_display_links = ["id", "user"]
+    list_filter = ["created_at", "updated_at"]
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.ArticleView, ArticleViewAdmin)
+admin.site.register(models.Clap, ClapAdmin)
--- core_apps/bookmarks/models.py
+++ core_apps/bookmarks/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from core_apps.articles.models import Article
+
+User = get_user_model()
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookmarks")
+    article = models.ForeignKey(
+        Article, on_delete=models.CASCADE, related_name="bookmarks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["user", "article"]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.first_name} bookmarked {self.article.title}"
