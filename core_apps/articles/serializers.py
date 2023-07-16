d0717e317bb87adffc07b292235a0ac2b6eb2677
elijah obara <elijahobara357@gmail.com>
2023-07-16 09:38:47 +0300

    feat: add article serialaizers

--- core_apps/articles/serializers.py
+++ core_apps/articles/serializers.py
@@ -1,1 +1,86 @@
-from rest_framework.serializers import serializers
+from rest_framework import serializers
+
+from core_apps.articles.models import Article, ArticleView, Clap
+from core_apps.bookmarks.models import Bookmark
+from core_apps.bookmarks.serializers import BookmarkSerializer
+from core_apps.profiles.serializers import ProfileSerializer
+from core_apps.responses.serializers import ResponseSerializer
+
+class TagListField(serializers.Field):
+    """takes a queryset of tags (value) and 
+    returns a list of tag names by iterating
+    over each tag and extracting its name using a list comprehension
+    """
+    def to_representation(self, value):
+        return [tag.name for tag in value.all()]
+    
+
+    def to_internal_value(self, data):
+        if not isinstance(data, list):
+            raise serializers.ValidationError("Expected a list of tags")
+
+        tag_objects = []
+        for tag_name in data:
+            tag_name = tag_name.strip()
+
+            if not tag_name:
+                continue
+            tag_objects.append(tag_name)
+        return tag_objects
+    
+class ArticleSerializer(serializers.ModelSerializer):
+    author_info = ProfileSerializer(source="author.profile", read_only=True)
+    banner_image = serializers.SerializerMethodField()
+    estimated_reading_time = serializers.ReadOnlyField()
+    tags = TagListField()
+    views = serializers.SerializerMethodField()
+    average_rating = serializers.ReadOnlyField()
+    bookmarks = serializers.SerializerMethodField()
+    bookmarks_count = serializers.SerializerMethodField()
+    claps_count = serializers.SerializerMethodField()
+    responses = ResponseSerializer(many=True, read_only=True)
+    responses_count = serializers.IntegerField(source="responses.count", read_only=True)
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
+
+    def get_responses_count(self, obj):
+        return obj.responses.count()
+
+    def get_claps_count(self, obj):
+        return obj.claps.count()
+
+    def get_bookmarks(self, obj):
+        bookmarks = Bookmark.objects.filter(article=obj)
+        return BookmarkSerializer(bookmarks, many=True).data
+
+    def get_bookmarks_count(self, obj):
+        return Bookmark.objects.filter(article=obj).count()
+
+    def get_average_rating(self, obj):
+        return obj.average_rating()
+
+    def get_views(self, obj):
+        return ArticleView.objects.filter(article=obj).count()
+
+    def get_banner_image(self, obj):
+        return obj.banner_image.url
+
+    def get_created_at(self, obj):
+        now = obj.created_at
+        formatted_date = now.strftime("%m/%d/%Y, %H:%M:%S")
+        return formatted_date
+
+    def get_updated_at(self, obj):
+        then = obj.updated_at
+        formatted_date = then.strftime("%m/%d/%Y, %H:%M:%S")
+        return formatted_date
+
+    def create(self, validated_data):
+        tags = validated_data.pop("tags")
+        article = Article.objects.create(**validated_data)
+        article.tags.set(tags)
+        return article
+    
+    def update(self, instance, validated_data):
+        
--- core_apps/articles/views.py
+++ core_apps/articles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.core.files.storage import default_storage
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, permissions, status
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+
+from .filters import ArticleFilter
+from .models import Article, ArticleView, Clap
+from .pagination import ArticlePagination
+from .permissions import IsOwnerOrReadOnly
+from .renderers import ArticleJSONRenderer, ArticlesJSONRenderer
+from .serializers import ArticleSerializer, ClapSerializer
+
