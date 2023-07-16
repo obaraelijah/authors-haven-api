205fe4f555053e0eeb315b8c15295fd3a113d6de
elijah obara <elijahobara357@gmail.com>
2023-07-16 13:14:29 +0300

    feat: add rating, exceptions and urls

--- authors_api/urls.py
+++ authors_api/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('supersecret/', admin.site.urls),
     path("api/v1/profiles/", include("core_apps.profiles.urls")),
     path("api/v1/articles/", include("core_apps.articles.urls")),
+    path("api/v1/ratings/", include("core_apps.ratings.urls")),
     path("api/v1/bookmarks/", include("core_apps.bookmarks.urls")),
     path("api/v1/responses/", include("core_apps.responses.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
