a525180e303eb14efe8ce12a0a631184d9222a20
elijah obara <elijahobara357@gmail.com>
2023-07-16 11:08:23 +0300

    feat: add views , and serializers

--- core_apps/articles/views.py
+++ core_apps/articles/views.py
@@ -16,3 +16,61 @@ from .permissions import IsOwnerOrReadOnly
 from .renderers import ArticleJSONRenderer, ArticlesJSONRenderer
 from .serializers import ArticleSerializer, ClapSerializer
 
+User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
+
+class ArticleListCreateView(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = ArticlePagination
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_class = ArticleFilter
+    ordering_fields = [
+        "created_at",
+        "updated_at",
+    ]
+    renderer_classes = [ArticlesJSONRenderer]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        logger.info(
+            f"article {serializer.data.get('title')} created by {self.request.user.first_name}"
+        )
+
+
+class ArticleRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    lookup_field = "id"
+    renderer_classes = [ArticleJSONRenderer]
+    parser_classes = [MultiPartParser, FormParser]
+    
+    def perform_update(self, serializer):
+        instance = serializer.save(author=self.request.user)
+        if "banner_image" in self.request.FILES:
+            if (
+                instance.banner_image
+                and instance.banner_image.name != "/profile_default.png"
+            ):
+                default_storage.delete(instance.banner_image.path)
+            instance.banner_image = self.request.FILES["banner_image"]
+            instance.save()
+            
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except Http404:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(instance)
+
+        viewer_ip = request.META.get("REMOTE_ADDR", None)
+        ArticleView.record_view(
+            article=instance, user=request.user, viewer_ip=viewer_ip
+        )
+
+        return Response(serializer.data)
