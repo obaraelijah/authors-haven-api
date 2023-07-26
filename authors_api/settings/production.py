f85424e2f79810d888963ccc0ee1101f7d8575c6
obaraelijah <elijahobara357@gmail.com>
2023-07-26 19:55:23 +0300

    prod: setup project for deployment

--- authors_api/settings/production.py
+++ authors_api/settings/production.py
@@ -1,2 +1,13 @@
 from .base import *  # noqa
 from .base import env
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = []
+
+DATABASES = {"default": env.db("DATABASE_URL")}
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SITE_NAME = "Authors Haven"
