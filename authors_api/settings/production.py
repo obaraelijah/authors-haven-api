a230319f5c9797cd9e6bf50e7b95f1d4aa991af2
obaraelijah <elijahobara357@gmail.com>
2023-08-14 12:54:57 +0300

    prod: setup project for deployment

--- authors_api/settings/production.py
+++ authors_api/settings/production.py
@@ -6,8 +6,47 @@ CSRF_TRUSTED_ORIGINS = []
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
 
-SITE_NAME = "Authors Haven"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "django.security.DisallowedHost": {
+            "handlers": ["console", "mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
