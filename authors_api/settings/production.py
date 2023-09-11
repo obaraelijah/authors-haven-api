fcec2e83cf875c4e161471e115a2208fe61b4730
obaraelijah <elijahobara357@gmail.com>
2023-09-11 11:10:37 +0300

    prod: setup project for deployment

--- authors_api/settings/production.py
+++ authors_api/settings/production.py
@@ -4,12 +4,19 @@ from .base import env
 ADMINS = [("Elijah Obara Samson", "elijahobara357@gmail.com")]
 
 # TODO add domain names of the production server
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["*"]
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["elijahobara357@gmail.com"])
+
+ADMIN_URL = env("DJANGO_ADMIN_URL")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 
 SESSION_COOKIE_SECURE = True
 
@@ -18,7 +25,37 @@ CSRF_COOKIE_SECURE = True
 # TODO: change to 518400 later
 SECURE_HSTS_SECONDS = 60
 
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+    "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
+)
+
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
+    "DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True
+)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_FROM_EMAIL = env(
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="Authors Haven Support <elijahobara357@gmail.com>",
+)
+
 SITE_NAME = "Authors Haven"
+
+SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
+
+EMAIL_SUBJECT_PREFIX = env(
+    "DJANGO_EMAIL_SUBJECT_PREFIX",
+    default="[Authors Haven]",
+)
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_HOST_USER = "elijahauthors@gmail.com"
+EMAIL_HOST_PASSWORD = env("SMTP_MAILGUN_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DOMAIN = env("DOMAIN")
 
 LOGGING = {
     "version": 1,
